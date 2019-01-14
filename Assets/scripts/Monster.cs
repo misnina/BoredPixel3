12fac6487c2f9e339ecb98e108fd03fa06a807d8
Nina <ninarachaebuie@gmail.com>
2019-01-13 20:54:06 -0600

    Add basic breeding setup

---
+++ Assets/scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Monster
+{
+    public string MonName;
+    public string Type;
+    public int Health;
+
+    public string Parent1Type;
+    public string Parent2Type;
+
+    public Monster(string monName, string type, int health, string parent1Type, string parent2Type)
+    {
+        MonName = monName;
+        Type = type;
+        Health = health;
+        Parent1Type = parent1Type;
+        Parent2Type = parent2Type;
+
+    }
+
+}
