36d85c33360098f054a375cb07a92fa833d48fa0
Nina <ninarachaebuie@gmail.com>
2019-01-19 11:30:35 -0600

    Add Ending

---
+++ Assets/scripts/GemLocation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GemLocation : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(0.5f, 45.5f), transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("win");
+        }
+    }
+}
